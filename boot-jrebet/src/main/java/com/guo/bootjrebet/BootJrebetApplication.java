9857fc1633028e29b5f4635f0c33c842e8c367e5
guo <11>
2018-06-08 17:31:20 +0800

    试试

---
+++ boot-jrebet/src/main/java/com/guo/bootjrebet/BootJrebetApplication.java
@@ -0,0 +1,12 @@
+package com.guo.bootjrebet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJrebetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJrebetApplication.class, args);
+	}
+}
