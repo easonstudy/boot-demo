9107e73aaef521e329866d36a50dab3c40f7e25b
guo <11>
2018-06-12 14:23:23 +0800

    试试

---
+++ boot-swagger2/src/main/java/com/guo/bootswagger2/BootSwagger2Application.java
@@ -0,0 +1,14 @@
+package com.guo.bootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class BootSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSwagger2Application.class, args);
+	}
+}
