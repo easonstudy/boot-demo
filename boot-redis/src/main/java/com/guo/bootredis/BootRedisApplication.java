e001036be827fa65b50e2c731e4a2196229f57d1
guo <11>
2018-06-15 18:12:50 +0800

    sso测试

---
+++ boot-redis/src/main/java/com/guo/bootredis/BootRedisApplication.java
@@ -0,0 +1,12 @@
+package com.guo.bootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRedisApplication.class, args);
+	}
+}
