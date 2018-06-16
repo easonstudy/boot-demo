7de26624444c58aed4b25da0b4aa45e7d52c70f2
guo <11>
2018-06-16 12:13:31 +0800

    sso测试

---
+++ boot-socket/src/main/java/com/guo/bootsocket/BootSocketApplication.java
@@ -0,0 +1,12 @@
+package com.guo.bootsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSocketApplication.class, args);
+	}
+}
