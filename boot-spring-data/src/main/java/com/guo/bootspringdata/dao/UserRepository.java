20fff1ace8b4bf860357f0b251e306657623b137
easonstudy <1161588342@qq.com>
2018-06-20 23:15:47 +0800

    spring data learn

---
+++ boot-spring-data/src/main/java/com/guo/bootspringdata/dao/UserRepository.java
@@ -0,0 +1,7 @@
+package com.guo.bootspringdata.dao;
+
+import com.guo.bootspringdata.pojo.User;
+import org.springframework.data.repository.Repository;
+
+public interface UserRepository extends Repository<User,Long>{
+}
